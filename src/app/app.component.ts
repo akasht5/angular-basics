af16f50d7799cd8de238d46492ec6ccb674de2a7
aditipuj <aditip@siddhatech.com>
2022-03-15 19:35:48 +0530

    Initial application basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpResponse, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-app';
+
+  private apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+  users:any = null;
+
+  constructor(private http: HttpClient){
+    console.log("Getting users data ...");
+    this.getUsers();
+  }
+    
+  getData(){
+    return this.http.get(this.apiUrl);
+  }
+
+  getUsers(){
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.users = data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
